b0593b67c447d943cfafcbb1678f3522bed3c4d4
nigussolomon <nigussolomon444@gmail.com>
2023-04-23 10:26:59 +0300

    added logout button

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,9 +3,17 @@ import logo from "../../logo.svg";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from "react-router-dom";
 import "./nav.css";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/")
+  };
   return (
     <>
       <AppBar
@@ -25,8 +33,9 @@ export default function NavBar() {
         className="nav"
         position="static"
       >
-        <Container maxWidth="xl" sx={{ height: "25px" }}>
+        <Container maxWidth="xl" sx={{display: 'flex', height: "35px", justifyContent: 'flex-end' ,alignItems: 'right',}}>
           <Toolbar disableGutters>
+          <Button onClick={handleSubmit}lick variant="contained" color="primary" type="submit" sx={{ backgroundColor: '#541718', '&:hover': { backgroundColor: '#541718' }, '&:focus': { backgroundColor: '#541718' } }} endIcon={<LogoutIcon />}>Logout</Button>
           </Toolbar>
         </Container>
       </AppBar>
